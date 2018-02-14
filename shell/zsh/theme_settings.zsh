b97363de7a6af7c1019647b4d31053264612ec3e
Tobias Sette <contato@tobias.ws>
2018-02-13 23:50:59 -0200

    Updates apt/sources.list.d and zsh

---
+++ shell/zsh/theme_settings.zsh
@@ -0,0 +1,46 @@
+# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+#ZSH_THEME="robbyrussell"
+# My theme is not in themes/ directory
+#ZSH_THEME="powerlevel9k/powerlevel9k"
+
+# Theme settings
+DEFAULT_USER="tobias"
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=3
+POWERLEVEL9K_SHORTEN_DELIMITER=".."
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir virtualenv vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status command_execution_time time)
+POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=3
+
+#POWERLEVEL9K_DIR_HOME_BACKGROUND="060"
+#POWERLEVEL9K_DIR_HOME_FOREGROUND="white"
+#POWERLEVEL9K_DIR_HOME_SUBFOLDER_BACKGROUND="060" # 060 070
+#POWERLEVEL9K_DIR_HOME_SUBFOLDER_FOREGROUND="white"
+#POWERLEVEL9K_DIR_DEFAULT_BACKGROUND="060"
+#POWERLEVEL9K_DIR_DEFAULT_FOREGROUND="white"
+#POWERLEVEL9K_DIR_WRITABLE_FORBIDDEN_BACKGROUND="060"
+#POWERLEVEL9K_DIR_WRITABLE_FORBIDDEN_FOREGROUND="255"
+
+#POWERLEVEL9K_VCS_CLEAN_FOREGROUND='white'
+#POWERLEVEL9K_VCS_UNTRACKED_FOREGROUND='white'
+#POWERLEVEL9K_VCS_MODIFIED_FOREGROUND='white'
+
+# Test!
+# https://github.com/bhilburn/powerlevel9k/wiki/Stylizing-Your-Prompt
+# Segment in black and white
+POWERLEVEL9K_LOAD_CRITICAL_BACKGROUND="black"
+POWERLEVEL9K_LOAD_CRITICAL_FOREGROUND="white"
+POWERLEVEL9K_LOAD_WARNING_BACKGROUND="black"
+POWERLEVEL9K_LOAD_WARNING_FOREGROUND="white"
+POWERLEVEL9K_LOAD_NORMAL_BACKGROUND="black"
+POWERLEVEL9K_LOAD_NORMAL_FOREGROUND="white"
+# Colorize only the visual identifier
+POWERLEVEL9K_LOAD_CRITICAL_VISUAL_IDENTIFIER_COLOR="red"
+POWERLEVEL9K_LOAD_WARNING_VISUAL_IDENTIFIER_COLOR="yellow"
+POWERLEVEL9K_LOAD_NORMAL_VISUAL_IDENTIFIER_COLOR="green"
+
+# https://github.com/bhilburn/powerlevel9k/wiki/Install-Instructions#step-2-install-a-powerline-font
+# https://github.com/gabrielelana/awesome-terminal-fonts#how-to-install-linux
+# https://github.com/bhilburn/powerlevel9k/wiki/About-Fonts
+#source ~/.local/share/fonts/awesome-terminal-fonts_maps
+
+
