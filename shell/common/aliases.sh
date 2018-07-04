3a5c62dd7ad85627bc7ea0179e1e6ca1869c9afd
Tobias Sette <contato@tobias.ws>
2018-07-03 21:18:13 -0300

    Add ifconfig alias

--- shell/common/aliases.sh
+++ shell/common/aliases.sh
@@ -33,4 +33,7 @@ alias dcompose='docker-compose'
 
 alias g="git"
 
+# show only hardware interfaces
+alias ifconfig='ls /sys/class/net | egrep -v "^(lo[0-9]?|sit[0-9]|ce[0-9]?|docker[0-9]?|br[-a-z0-9]{13})$" | xargs --max-args=1 /sbin/ifconfig'
+
 #EOF
