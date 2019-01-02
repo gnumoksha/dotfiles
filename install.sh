e05ac22608b16960a316adc44af8eeac1081aba4
Tobias Sette <contato@tobias.ws>
2019-01-02 15:59:48 -0200

    Move installation files and gvimrc

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 STOW="$(which stow) --verbose=0"
 export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:=${HOME}/.config}
 
-source shell-install.sh
-source gui-install.sh
+source shell/install.sh
+source gui/install.sh
 
 echo "Finished."
 
