8219c440ea465f17bdefb0e1f9dd24e64859ca1d
Tobias Sette <contato@tobias.ws>
2019-01-15 19:23:22 -0200

    Add iPython/Jupyter configs

--- shell/home/config/shell/common/bootstrap.sh
+++ shell/home/config/shell/common/bootstrap.sh
@@ -9,5 +9,6 @@ source $current_dir/colors.sh
 source $current_dir/ssh.sh
 source $current_dir/containers.sh
 source $current_dir/fzf.sh
+source $current_dir/jupyter.sh
 
 #EOF
---
+++ shell/home/config/shell/common/jupyter.sh
@@ -0,0 +1,7 @@
+
+# Use XDG. See:
+# * https://github.com/jupyter/notebook/issues/2374
+# * https://wiki.archlinux.org/index.php/XDG_Base_Directory
+export IPYTHONDIR="$XDG_CONFIG_HOME/ipython"
+export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME/jupyter"
+
