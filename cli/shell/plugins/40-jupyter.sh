820d7b9fd5bdbf5c57e5e7d4e0b4cb44c1ad40fd
Tobias Sette <me@tobias.ws>
2020-07-05 11:17:42 -0300

    refactor: remove personal plugins

--- cli/shell/plugins/40-jupyter.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Use XDG. See:
-# * https://github.com/jupyter/notebook/issues/2374
-# * https://wiki.archlinux.org/index.php/XDG_Base_Directory
-export IPYTHONDIR="$XDG_CONFIG_HOME/ipython"
-export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME/jupyter"
-
