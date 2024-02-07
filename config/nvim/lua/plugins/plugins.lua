fa7360a5fc161a43cee3b9396d567e3b0256dd79
Tobias Sette <me@tobias.ws>
2024-02-06 22:53:42 -0300

    refactor: use nvim as default editor

---
+++ config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,13 @@
+return {
+  -- "tanvirtin/monokai.nvim"
+  -- "Shatur/neovim-ayu"
+  { "tomasr/molokai" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- tokyonight-storm
+      colorscheme = "molokai",
+    },
+  },
+}
