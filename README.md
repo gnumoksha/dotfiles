acbfdf325996431503299bfc9ad794c7659ac736
Tobias Sette <me@tobias.ws>
2026-03-05 05:14:20 -0300

    chore: move to GitHub (#2)

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # dotfiles
-![pipeline](https://gitlab.com/gnumoksha/dotfiles/badges/master/pipeline.svg?style=flat-square)
-![coverage](https://gitlab.com/gnumoksha/dotfiles/badges/master/coverage.svg?style=flat-square)
+![Build Status](https://github.com/gnumoksha/dotfiles/actions/workflows/test.yml/badge.svg)
 
 Configuration files for command line and some graphical softwares.
 
 ## Installation
 ```bash
 export DOTFILES=$HOME/.local/share/dotfiles
-git clone https://gitlab.com/gnumoksha/dotfiles.git $DOTFILES
+git clone git@github.com:gnumoksha/dotfiles.git $DOTFILES
 cd $DOTFILES
 ./install.sh
 ```
@@ -15,7 +14,3 @@ cd $DOTFILES
 ## Rules
 * [If possible](https://wiki.archlinux.org/index.php/XDG_Base_Directory#Support), configuration files will follow [XDG Base Directory Specification](https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html).
 * A descriptive name is better than a short name.
-
-## Warning
-* Please, note that github repository is only a mirror, the main efforts in this code are submitted to [gitlab](https://gitlab.com/gnumoksha/dotfiles).
-
--- shell/plugins/00-settings.sh
+++ shell/plugins/00-settings.sh
@@ -5,7 +5,7 @@
 #
 # Disables Software Flow Control (XON/XOFF flow control)
 # i.e. "Ctrl s" and "Ctrl q" will have no special behavior.
-[[ $- == *i*  ]] && stty -ixon <$TTY >$TTY
+[[ $- == *i* ]] && stty -ixon <$TTY >$TTY
 
 #
 # For Tilix.
@@ -18,30 +18,6 @@ if [ "$TILIX_ID" ] && [ "$VTE_VERSION" ]; then
   source /etc/profile.d/vte.sh
 fi
 
-# Remove terminal borders
-#if [ "$TERM" = "xterm-256color" ]; then
-  #xprop \
-    #-id $(xdotool getactivewindow) \
-    #-f _MOTIF_WM_HINTS 32c \
-    #-set _MOTIF_WM_HINTS "0x2, 0x0, 0x0, 0x0, 0x0"
-#fi
-
-# setup the command-not-found package
-if [[ -x /usr/lib/command-not-found ]] ; then
-  export COMMAND_NOT_FOUND_INSTALL_PROMPT=1
-
-  if [[ -n "$ZSH_VERSION" ]]; then
-    # The following code was adapted from /etc/zsh_command_not_found
-    # present on Debian:
-    if (( ! ${+functions[command_not_found_handler]} )) ; then
-      function command_not_found_handler {
-        [[ -x /usr/lib/command-not-found ]] || return 1
-        /usr/lib/command-not-found -- ${1+"$1"} && :
-      }
-    fi
-  fi
-fi
-
 #export PAGER=most
 # https://github.com/rkitover/vimpager
 #export PAGER=/opt/apps/vimpager/vimpager
@@ -51,4 +27,3 @@ export PAGER=less
 export LESS='-g -i -M -R -S -w -z-4'
 
 export PATH="$PATH:$DOTFILES/bin"
-
