4f7934af01fff613cced0a94765b7b53b7932d2c
Tobias Sette <me@tobias.ws>
2026-03-15 00:02:36 -0300

    fix: some errors when running bash

--- shell/plugins/00-settings.sh
+++ shell/plugins/00-settings.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # Terminal settings
-# shellcheck disable=SC1091
 
-#
 # Disables Software Flow Control (XON/XOFF flow control)
 # i.e. "Ctrl s" and "Ctrl q" will have no special behavior.
 # shellcheck disable=SC2094
-[[ $- == *i* ]] && stty -ixon <"$TTY" >"$TTY"
+if [ -n "$ZSH_VERSION" ]; then
+	[[ $- == *i* ]] && stty -ixon <"$TTY" >"$TTY"
+else
+	[[ $- == *i* ]] && stty -ixon
+fi
